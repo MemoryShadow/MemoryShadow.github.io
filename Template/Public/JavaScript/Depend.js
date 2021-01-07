859ce0e51609dbca98a248eced2cc41decd55581
MemoryShadow <2320667376@qq.com>
2021-01-08 01:23:45 +0800

    更新: Depend.js已经可以正常工作,可用于加载依赖项

--- Template/Public/JavaScript/Depend.js
+++ Template/Public/JavaScript/Depend.js
@@ -2,13 +2,13 @@
  * @Date         : 2021-01-07 23:18:20
  * @Author       : MemoryShadow
  * @LastEditors  : MemoryShadow
- * @LastEditTime : 2021-01-08 00:32:33
+ * @LastEditTime : 2021-01-08 01:19:04
  * @Description  : 用于处理依赖的Depend类
  */
 
 /**
  * 将指定资源添加至加载队列中并加载,用于处理依赖
- * @param {String} ResourcesURL     指定资源URL
+ * @param {String} ResourcesURL 指定资源URL
  */
 function Depend(ResourcesURL) {
     // 此资源的URL
@@ -17,6 +17,8 @@ function Depend(ResourcesURL) {
     this.ResourcesType;
     // 此对象在公共队列中的位置
     this.thisObjectIndex;
+    // 节点对象
+    this.NodeObj = undefined;
 
     /**
      * 解析当前URL的类型
@@ -28,24 +30,54 @@ function Depend(ResourcesURL) {
     }
 
     /**
+     * 注册当前资源
+     * @returns {HTMLElement} 已经成功注册的HTMLElement对象
+     */
+    this.RegisteredNode = function (e) {
+        switch (this.ResourcesType) {
+            case 'js':
+                let script = document.createElement("script");
+                script.src = this.ResourcesURL;
+                this.NodeObj = document.head.appendChild(script);
+                break;
+            case 'css':
+                let link = document.createElement("link");
+                link.href = this.ResourcesURL;
+                link.type = "text/css";
+                link.rel = "stylesheet";
+                this.NodeObj = document.head.appendChild(link);
+                break;
+
+            default:
+                console.error("暂不支持这个类型的资源");
+                return undefined;
+        }
+        // 记录当前的控制组件
+        return this.NodeObj;
+    }
+
+    /**
     * 构造器
-    * @param {String} ResourcesURL     指定资源URL
+    * @param {String} ResourcesURL 指定资源URL
     */
     this.constructor = function (ResourcesURL) {
+        // 储存URL
         this.ResourcesURL = ResourcesURL;
+        // 自动推导资源类型
         this.ResourcesType = this.AnalysisResourcesType();
+        // 记录当前索引位置
         this.thisObjectIndex = Depend.ObjectList.length;
         // 将自身添加至对象列
         Depend.ObjectList.push(this);
     }
 
-    this.constructor(ResourcesURL, ResourcesType);
+    this.constructor(ResourcesURL);
 }
 
 /**
  * 索引
  * @param {int,String} Key 要用于索引的Key
- * @return {ControlNode} 指定的控制对象
+ * @return {Depend} 指定的控制对象
  */
 Depend.indexOf = function (Key) {
     // 判断是数字还是字符串
@@ -69,16 +101,16 @@ Depend.indexOf = function (Key) {
 /**
  * 获取指定节点的控制对象
  * @param {string} MainNodeID 指定要作为主节点的ID
- * @return {ControlNode} 指定的控制对象
+ * @return {Depend} 指定的控制对象
  */
 Depend.valueOf = function (index) {
     // 检查是否存在重复的对象,如果有,就直接返回,没有的情况下才创建新对象
-    if (ControlNode.indexOf(index) == undefined) {
+    if (Depend.indexOf(index) == undefined) {
         // 如果没有,就New新的值
-        return new ControlNode(index);
+        return new Depend(index);
     } else {
         // 如果不为空,就返回指定的内容
-        return ControlNode.indexOf(index);
+        return Depend.indexOf(index);
     }
 }
 
