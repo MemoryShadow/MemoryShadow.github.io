95ea67a7aaf043147bb57eab40bee36f3425e37c
MemoryShadow <31596045+MemoryShadow@users.noreply.github.com>
2023-04-08 10:17:05 +0000

    feat: 新增QRDE, 修复页面无法访问的问题

---
+++ QRDE.sh
@@ -0,0 +1,11 @@
+###
+ # @Date: 2023-04-08 10:04:16
+ # @LastEditors: MemoryShadow
+ # @LastEditTime: 2023-04-08 10:04:23
+ # @Description: 快速构建开发环境
+ # Copyright (c) 2023 by MemoryShadow@outlook.com, All Rights Reserved.
+### 
+git submodule update --init
+npm install hexo-cli -g
+npm install
+hexo server
